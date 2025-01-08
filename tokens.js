d32bc633f5c0ab19f844f9715a42e8290d6e88d9
jasper-clarke <me@jasperclarke.com>
2025-01-09 10:05:51 +1100

    implementation

---
+++ tokens.js
@@ -0,0 +1,9 @@
+const TOKENS = [
+  {
+    username: "sschobertdev",
+    token:
+      "IGAAPwxyGos7NBZAE9hLWVpYzU4bHY5anp1TjdFa3M4ZAFQzaURiQVpkczEyYkVDLVdJbDV2SjNyVzRoOGh5VXN5cTB5SHFVNm5oZADk3Mks4dG1KNXY4ZAXpiLWRqZAXZAHVC1SSzhhR09Sb1EtSTJYMVdNZAUx3",
+  },
+];
+
+export { TOKENS };
